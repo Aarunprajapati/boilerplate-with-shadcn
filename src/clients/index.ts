e876cf04b7c2b40ff9389d94a7cda76629313965
Arun Prajapati <arunprajapati8502@gmail.com>
2026-04-29 23:50:17 +0530

    feat: lint error fixed

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -5,13 +5,13 @@ const org = envConfig.org_name;
 
 const isCustomOrg = org && org !== "default";
 
-export const Mainlayout = isCustomOrg ? lazy(()=>import(`./${org}/main/MainLayout`)) : lazy(()=>import(`./default/main/DefaultMainLayout`))
+export const Mainlayout = isCustomOrg ? lazy(() => import(`./${org}/main/MainLayout`)) : lazy(() => import(`./default/main/DefaultMainLayout`))
 
-export const SideBarMenu = isCustomOrg ? lazy(()=>import(`./${org}/main/components/SideBarMenu`)) : lazy(()=>import(`./default/main/components/SideBarMenu`))
+export const SideBarMenu = isCustomOrg ? lazy(() => import(`./${org}/main/components/SideBarMenu`)) : lazy(() => import(`./default/main/components/SideBarMenu`))
 
 
-export const Dashboardlayout = isCustomOrg ? lazy(()=>import(`./${org}/dashboard/DashboardLayout`)) : lazy(()=>import(`./default/dashboard/DefaultDashboardLayout`))
+export const Dashboardlayout = isCustomOrg ? lazy(() => import(`./${org}/dashboard/DashboardLayout`)) : lazy(() => import(`./default/dashboard/DefaultDashboardLayout`))
 
-export const LoginLayout = isCustomOrg ? lazy(()=>import(`./${org}/login/LoginLayout`)) : lazy(()=>import(`./default/login/DefaultLoginLayout`))
+export const LoginLayout = isCustomOrg ? lazy(() => import(`./${org}/login/LoginLayout`)) : lazy(() => import(`./default/login/DefaultLoginLayout`))
 
-export const ThemeProvider = isCustomOrg ? lazy(()=>import(`./${org}/styles/ThemeProvider`)) : lazy(()=>import(`./default/styles/ThemeProvider`))
+export const ThemeProvider = isCustomOrg ? lazy(() => import(`./${org}/styles/ThemeProvider`)) : lazy(() => import(`./default/styles/ThemeProvider`))
