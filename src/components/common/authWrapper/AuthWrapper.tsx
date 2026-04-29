b75a584e2e41125e9d31597fdbd7ebbb389e5800
Arun Prajapati <arunprajapati8502@gmail.com>
2026-04-30 00:27:43 +0530

    login disabled

--- src/components/common/authWrapper/AuthWrapper.tsx
+++ src/components/common/authWrapper/AuthWrapper.tsx
@@ -1,20 +1,22 @@
-import { LocalStorageConfig } from '@/config/router/LocalStorageConfig';
-import { RouterKeys } from '@/config/router/RouterKeys';
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+// import { LocalStorageConfig } from '@/config/router/LocalStorageConfig';
+// import { RouterKeys } from '@/config/router/RouterKeys';
+// import { Navigate, Outlet, useLocation } from 'react-router-dom';
+
+import { Outlet } from "react-router-dom";
 
 
 const AuthWrapper = () => {
-  const token = LocalStorageConfig.getToken();
-  const { pathname } = useLocation();
+  // const token = LocalStorageConfig.getToken();
+  // const { pathname } = useLocation();
 
 
-if (token && pathname == `/${RouterKeys.COMMON.LOGIN}`) {
-    return <Navigate to={`/${RouterKeys.DASHBOARD}`} replace />;
-  }
+// if (token && pathname == `/${RouterKeys.COMMON.LOGIN}`) {
+//     return <Navigate to={`/${RouterKeys.DASHBOARD}`} replace />;
+//   }
 
-  if (!token && pathname !== `/${RouterKeys.COMMON.LOGIN}`) {
-    return <Navigate to={`/${RouterKeys.COMMON.LOGIN}`} replace />;
-  }
+//   if (!token && pathname !== `/${RouterKeys.COMMON.LOGIN}`) {
+//     return <Navigate to={`/${RouterKeys.COMMON.LOGIN}`} replace />;
+//   }
 
   return <Outlet />
 };
